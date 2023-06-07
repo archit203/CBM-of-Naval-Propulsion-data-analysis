0f891f9cccf0b552b1b75a4b9a1e6dcf8ff2eade
Archit Gupta <63711170+archit203@users.noreply.github.com>
2023-06-08 00:08:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pickle
 import numpy as np
 
-model = pickle.load(open(r"naval.pkl", 'rb'))
+model = pickle.load(open(r'naval.pkl', 'rb'))
 
 st.title('CBM naval Predictor')
 
